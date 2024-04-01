2d5c7daf2df6ada0c81a6ad97d65f718daf0f977
Raja Zarrouki <127046732+RajaZarrouki@users.noreply.github.com>
2024-04-01 22:17:13 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Dollars dollars = new Dollars();
+        Euros euros = new Euros();
+
+        double amountInDollars = 100;
+        double amountInEuros = dollars.convertTo(amountInDollars, euros);
+        System.out.println(amountInDollars + " USD = " + amountInEuros + " EUR");
+
+        Dinars dinars = new Dinars();
+        double amountInDinars = dollars.convertTo(amountInDollars, dinars);
+        System.out.println(amountInDollars + " USD = " + amountInDinars + " DZD");
+    }
+}
 
